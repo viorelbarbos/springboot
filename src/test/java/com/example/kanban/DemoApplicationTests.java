fc29dfea1051aead1fdd2bdba83cb1226904dde7
viorelbarbos <72337534+viorelbarbos@users.noreply.github.com>
2024-01-23 18:52:06 +0000

    Update project details in pom.xml

--- src/test/java/com/example/kanban/DemoApplicationTests.java
+++ src/test/java/com/example/kanban/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.kanban;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class KanbanBackendTests {
 
 	@Test
 	void contextLoads() {
