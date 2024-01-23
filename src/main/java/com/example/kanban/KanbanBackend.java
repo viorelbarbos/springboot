26f0f8a9b5c28fa7ea864a09b4ecb897bf264a80
viorelbarbos <72337534+viorelbarbos@users.noreply.github.com>
2024-01-23 18:52:31 +0000

    Remove unnecessary imports

--- src/main/java/com/example/kanban/KanbanBackend.java
+++ src/main/java/com/example/kanban/KanbanBackend.java
@@ -1,10 +1,6 @@
 package com.example.kanban;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 public class KanbanBackend {
 
